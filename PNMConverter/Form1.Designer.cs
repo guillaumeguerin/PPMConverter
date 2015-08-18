9466773d3578ff09b3efe46b1c1c882d592310be
guillaumeguerin <guerin_guillaume@hotmail.fr>
2015-08-18 20:06:48 +0200

    Added awesome icon for the application

--- PNMConverter/Form1.Designer.cs
+++ PNMConverter/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBoxPath = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonBrowse = new System.Windows.Forms.Button();
@@ -114,8 +115,9 @@
             this.Controls.Add(this.buttonBrowse);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxPath);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "PpmConverter";
+            this.Text = "PNMConverter";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
